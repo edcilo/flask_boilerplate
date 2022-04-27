3c3a962bc03a56a62c2a14b301a2f9e461febd0b
edcilo <edcilop@gmail.com>
2022-04-27 09:14:13 -0500

    fix make seeder command

--- ms/commands/makeSeeder.py
+++ ms/commands/makeSeeder.py
@@ -4,23 +4,27 @@ import re
 from flask.cli import with_appcontext
 
 
-@click.command(name='make:serializer',
-               help='Create a new serializer class')
-@click.option('-n', '--name', required=True, help='The name of the serializer')
+@click.command(name='make:seeder',
+               help='Create a new seeder class')
+@click.option('-n', '--name', required=True, help='The name of the seeder')
 @with_appcontext
-def makeserializer(name):
+def makeseeder(name):
     appPath = os.path.realpath('')
-    serializerPath = 'ms/serializers'
+    seederPath = 'ms/db/seeders'
     filename = f"{name[0].lower()}{name[1:]}"
-    fullpath = os.path.join(appPath, serializerPath, f"{filename}.py")
+    fullpath = os.path.join(appPath, seederPath, f"{filename}.py")
 
     seeder = open(fullpath, 'w+')
-    seeder.write(f'''from .serializer import Serializer
+    seeder.write(f'''from faker import Faker
+from flask_seeder import Seeder, generator
 
 
-class {name}(Serializer):
-    response = {{
-        "id": str,
-    }}
+class {name}(Seeder):
+    def __init__(self, db=None):
+        super().__init__(db=db)
+        self.priority = 10
+
+    def run(self):
+        fake = Faker()
 ''')
     seeder.close()
