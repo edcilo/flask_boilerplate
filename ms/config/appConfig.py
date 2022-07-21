ccac0110811c4f6c6e71c952b0298990f2db26ed
edcilo-bigsmart <eduardo.cifuentes@bigsmart.mx>
2022-07-21 16:48:22 -0500

    fix env var

--- ms/config/appConfig.py
+++ ms/config/appConfig.py
@@ -5,5 +5,5 @@ app_config = {
     'APP_NAME': os.getenv('APP_NAME', 'ms'),
     'APP_VERSION': os.getenv('APP_VERSION', '0.0.0'),
     'SECRET_KEY': os.getenv('APP_SECRET_KEY', None),
-    'TIMEZONE': os.getenv('TIMEZONE', 'UTC')
+    'TIMEZONE': os.getenv('APP_TIMEZONE', 'UTC')
 }
