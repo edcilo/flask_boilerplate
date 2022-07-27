3691a9d53e1cc475c3336bc7378d17148022146a
edcilo <edcilop@gmail.com>
2022-07-27 11:50:24 -0500

    add created_at attribtue to serializer templtate

--- ms/commands/templates/serializerTemplate.py
+++ ms/commands/templates/serializerTemplate.py
@@ -1,7 +1,9 @@
+from ms.helpers import time
 from ms.serializers import Serializer
 
 
 class <CLASSNAME>(Serializer):
     response = {
         "id": str,
+        "created_at": time.datetime_to_epoch,
     }
